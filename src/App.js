766b04c5b770e2a30702ca31bb7da24fcdaab371
RDK <rickykeisling@gmail.com>
2019-11-07 21:12:07 -0600

    Just mess around with some functions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,54 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  var helloWorld = 'Welcome to Old World Blues'
+  var first = "Rick";
+  var last = "Dee";
+  var greet = "Morning";
+  var mood = "confused";
+
   return (
     <div className="App">
-      <h2>{helloWorld}</h2>
+      <h1>{GetEnterMessage(GetGreeting(greet),
+           GetNameTogether(first, last),
+           GetMoodIndicator(mood))}</h1>
     </div>
   );
+}
+
+function GetEnterMessage(funcA, funcB, funcC){
+  return funcA + ' ' + funcB + funcC;
+}
+
+function GetNameTogether(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+function GetGreeting(greeting) {
+  switch (greeting) {
+    case "Hello":
+      return "Hello, ";
+    case "Morning":
+      return "Good morning, ";
+    default:
+      return "Hiya, ";
+  }
+}
+
+function GetMoodIndicator(mood){
+  var indicator = "";
+  if (mood === "excited") {
+    indicator = "!";
+  }
+  else if (mood === "normal") {
+    indicator = ".";
+  }
+  else if (mood === "confused") {
+    indicator = "?";
+  }
+  else {
+    indicator = "...";
+  }
+  return indicator;
 }
 
 export default App;
