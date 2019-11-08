9015b572d10ef51a600786f407478e4f17e9ff4e
RDK <rickykeisling@gmail.com>
2019-11-07 22:00:30 -0600

    Split out component into smaller chunks

--- src/App.js
+++ src/App.js
@@ -20,22 +20,34 @@ function App() {
 function Post(props) {
   return (
     <div className="Post">
-      <div className="UserInfo">
-        <img className="Avatar">
-          src={props.author.avatarUrl}
-          alt={props.auther.name}
-        </img>
-        <div className='UserInfo-name'>
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo user={props.author} />
       <div className="Post-text">
         {props.text}
       </div>
       <div className="Post-date">
-        {formatDate(props.date)}
+        {props.date}
       </div>
     </div>
+  );
+}
+
+function UserInfo(props){
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
   );
 }
 
